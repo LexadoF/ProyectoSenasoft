42c8c2858243884227312f78571c7948dae6edc7
LeonelBaena <leonelbaena2@gmail.com>
2020-10-14 09:24:11 -0500

    Creacion Inical del Login
    se sobre escribe el archivo por defecto remplazandolo por la primera version del login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  login = {
+    usuario: null,
+    contrasena: null
+  };
+
+  constructor( private loginService: LoginService){
+
+  }
+  loginUsuario(){
+    this.loginService.loginUsuario(this.login).subscribe (
+      datos => {
+        if(datos[ "resultado" ] == 'OK'){
+          alert(datos['Mensaje']);
+        } else {
+          alert(datos['Mensaje']);
+        }
+      }
+    );
+  }
 }
