000b8c52f8e703a3a06a79b6235c64baf18ea780
santy1205 <38736233+santy1205@users.noreply.github.com>
2020-10-14 17:58:16 -0500

    crecion y actualizacion de archivos

--- backend/app/Http/Controllers/CategoriasController.php
+++ backend/app/Http/Controllers/CategoriasController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\categorias;
 class CategoriasController extends Controller
-{
-    //
+{   
+    public function vista(){
+        return view('registro');
+    }
+    public function create(Request $request){
+        $categorias = new categorias();
+
+        $categorias -> Nombre = $request -> Nombre;
+        $categorias -> NitEmpresa = $request -> NitEmpresa;
+        $categorias -> save();
+
+        return redirect('/create');
+
+    }
 }
