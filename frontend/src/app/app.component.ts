026adffbc38248233bc458b3b70f07da71cc312a
Steve Fox <stevefoly@gmail.com>
2020-10-15 10:52:16 -0500

    Revert "creando los metodos de login y registro"
    This reverts commit ceeacff075778119ae678640917a0cc95efc889b.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,38 +1,30 @@
 import { Component } from '@angular/core';
-import { ApiService } from '../app/services/api.service';
+import { LoginService } from './services/login.service';
 
 @Component({
-selector: 'app-root',
-templateUrl: './app.component.html',
-styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-loginbtn:boolean;
-logoutbtn:boolean;
+  login = {
+    usuario: null,
+    contrasena: null
+  };
 
-constructor(private dataService: ApiService) {
-dataService.getLoggedInName.subscribe(name => this.changeName(name));
-if(this.dataService.isLoggedIn())
-{
-console.log("loggedin");
-this.loginbtn=false;
-this.logoutbtn=true
-}
-else{
-this.loginbtn=true;
-this.logoutbtn=false
-}
+  constructor( private loginService: LoginService ){
 
-}
 
-private changeName(name: boolean): void {
-this.logoutbtn = name;
-this.loginbtn = !name;
-}
-logout()
-{
-this.dataService.deleteToken();
-window.location.href = window.location.href;
-}
+  }
+  loginUsuario(){
+    this.loginService.loginUsuario(this.login).subscribe (
+      datos => {
+        if(datos[ "resultado" ] == 'OK'){
+          alert(datos['Mensaje']);
+        } else {
+          alert(datos['Mensaje']);
+        }
+      }
+    );
+  }
 }
