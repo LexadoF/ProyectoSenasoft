f6d74d53bcfffcf6695e231fbd55bbd9fa56ae62
Steve Fox <stevefoly@gmail.com>
2020-10-15 10:42:15 -0500

    Correccion y prueba de base de datos

--- database/facturacion.sql
+++ database/facturacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-10-2020 a las 17:16:39
+-- Tiempo de generación: 15-10-2020 a las 17:41:06
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.2.33
 
@@ -33,6 +33,13 @@ CREATE TABLE `categorias` (
   `NitEmpresa` int(11) NOT NULL COMMENT 'nit de la empresa a la que pertenece la categoria'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`Id`, `Nombre`, `NitEmpresa`) VALUES
+(1, 'cat1', 2324342);
+
 -- --------------------------------------------------------
 
 --
@@ -44,8 +51,15 @@ CREATE TABLE `cliente` (
   `NombreCliente` varchar(100) NOT NULL COMMENT 'Nombre del cliente',
   `TelefonoCliente` varchar(100) NOT NULL COMMENT 'Telefono del cliente',
   `DireccionCliente` varchar(50) NOT NULL COMMENT 'Direccion del cliente, se utilizara en caso de que se necesite',
-  `Estado` tinyint(1) NOT NULL COMMENT 'Estado del cliente, puede estar habilitado o deshabilitado'
+  `Estado` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'Estado del cliente, puede estar habilitado o deshabilitado'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`DocCliente`, `NombreCliente`, `TelefonoCliente`, `DireccionCliente`, `Estado`) VALUES
+('1234678', 'ddddd', '34444', 'sfdsg', 1);
 
 -- --------------------------------------------------------
 
@@ -55,12 +69,20 @@ CREATE TABLE `cliente` (
 
 CREATE TABLE `datosproductofactura` (
   `id` int(11) NOT NULL COMMENT 'Identificador unico del detalle de productos y factura',
-  `idProducto` int(11) NOT NULL COMMENT 'Identificador del producto el cual se agregara',
-  `idFactura` int(11) NOT NULL COMMENT 'Identificador de la factura a la cual pertenece este detalle',
+  `idProducto` int(11) NOT NULL DEFAULT 0 COMMENT 'Identificador del producto el cual se agregara',
+  `idFactura` int(11) NOT NULL DEFAULT 0 COMMENT 'Identificador de la factura a la cual pertenece este detalle',
   `CantidadProd` int(11) NOT NULL DEFAULT 1 COMMENT 'Cantidad seleccionada de este producto para esta factura',
   `PrecioUPr` double NOT NULL DEFAULT 0 COMMENT 'Valor unitario del producto que se utilizara para obtener luego el valor total ',
-  `SubTot` int(11) NOT NULL COMMENT 'Valor subtotal calculado de la cantidad de productos por el precio unitario de los mismos'
+  `SubTot` double NOT NULL DEFAULT 0 COMMENT 'Valor subtotal calculado de la cantidad de productos por el precio unitario de los mismos',
+  `Total` double NOT NULL DEFAULT 0 COMMENT 'Valor total de la factura, calculado mediante los datos consultados de datosfacturaproducto sumando todos los valores unitarios'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `datosproductofactura`
+--
+
+INSERT INTO `datosproductofactura` (`id`, `idProducto`, `idFactura`, `CantidadProd`, `PrecioUPr`, `SubTot`, `Total`) VALUES
+(1, 1, 1, 1, 5000, 5000, 0);
 
 -- --------------------------------------------------------
 
@@ -75,6 +97,13 @@ CREATE TABLE `empresa` (
   `Telefono` varchar(25) NOT NULL COMMENT 'Telefono principal de contacto con la empresa'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `empresa`
+--
+
+INSERT INTO `empresa` (`Nit`, `Nombre`, `Direccion`, `Telefono`) VALUES
+(2324342, 'aaaaaaa', 'aaaaaaa', '33333333');
+
 -- --------------------------------------------------------
 
 --
@@ -85,10 +114,17 @@ CREATE TABLE `factura` (
   `id` int(11) NOT NULL COMMENT 'Identificador unico de la factura',
   `DocCliente` varchar(30) NOT NULL DEFAULT ' ' COMMENT 'Documento del cliente a quien se le realiza la factura',
   `DocPersonaEncargada` varchar(30) NOT NULL DEFAULT ' ' COMMENT 'Documento del empleado o encargado que genera esta factura',
-  `fecha` date NOT NULL DEFAULT current_timestamp() COMMENT 'fecha de la factura, por defecto es la fecha actual',
-  `Valor total` double NOT NULL DEFAULT 0 COMMENT 'Valor total de la factura, calculado mediante los datos consultados de datosfacturaproducto sumando todos los valores unitarios',
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'fecha de la factura, por defecto es la fecha actual',
   `estado` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'estado de la factura, la cual es en espera en caso de no contarse con stock'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `factura`
+--
+
+INSERT INTO `factura` (`id`, `DocCliente`, `DocPersonaEncargada`, `fecha`, `estado`) VALUES
+(1, '1234678', '567895', '0000-00-00 00:00:00', 1),
+(2, '1234678', '567895', '2020-10-15 15:39:22', 1);
 
 -- --------------------------------------------------------
 
@@ -104,8 +140,15 @@ CREATE TABLE `persona` (
   `Correo` varchar(50) NOT NULL COMMENT 'Correo de la persona, no se pueden registrar 2 personas con el mismo correo',
   `Contrasena` varchar(100) NOT NULL COMMENT 'clave de la cuenta de esta persona',
   `NitEmpresa` int(11) NOT NULL COMMENT 'Nit de la empresa a la que pertenece el empleado o encargado',
-  `estado` tinyint(1) NOT NULL COMMENT 'estado de la persona en el sistema, puede ser activo o inactivo'
+  `estado` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'estado de la persona en el sistema, puede ser activo o inactivo'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`Documento`, `Nombre`, `Direccion`, `Telefono`, `Correo`, `Contrasena`, `NitEmpresa`, `estado`) VALUES
+('567895', 'jose', 'dir 1', '1234567', 'alguien@example.com', '123', 2324342, 1);
 
 -- --------------------------------------------------------
 
@@ -115,11 +158,18 @@ CREATE TABLE `persona` (
 
 CREATE TABLE `productos` (
   `id` int(11) NOT NULL COMMENT 'Identificador individual de cada producto',
-  `Nombre` int(11) NOT NULL COMMENT 'Refiere al nombre del producto',
+  `Nombre` varchar(50) NOT NULL COMMENT 'Refiere al nombre del producto',
   `stock` int(11) NOT NULL COMMENT 'Indica cuantos productos se encuentran en stock actualmente',
   `Precio` double NOT NULL COMMENT 'precio del producto por unidad, admite decimales en caso de ser necesario',
   `IdCategoria` int(11) NOT NULL COMMENT 'categoria a la que pertenece el producto'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `Nombre`, `stock`, `Precio`, `IdCategoria`) VALUES
+(1, 'prod 1', 50, 1000, 1);
 
 --
 -- Índices para tablas volcadas
@@ -184,25 +234,25 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador de la tabla Categoria';
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador de la tabla Categoria', AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `datosproductofactura`
 --
 ALTER TABLE `datosproductofactura`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador unico del detalle de productos y factura';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador unico del detalle de productos y factura', AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `factura`
 --
 ALTER TABLE `factura`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador unico de la factura';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador unico de la factura', AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador individual de cada producto';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador individual de cada producto', AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
