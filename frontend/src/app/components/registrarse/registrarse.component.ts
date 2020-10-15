aac8dcb8baae0e5fce91e38a4e48cf847e906ccc
LeonelBaena <leonelbaena2@gmail.com>
2020-10-15 12:27:13 -0500

    registro

--- frontend/src/app/components/registrarse/registrarse.component.ts
+++ frontend/src/app/components/registrarse/registrarse.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { FormGroup, FormControl, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-registrarse',
@@ -7,13 +10,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./registrarse.component.css']
 })
 export class RegistrarseComponent implements OnInit {
-  path = 'inicio';
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
+  reForm: FormGroup;
+  constructor(private fb: FormBuilder, private dataService: ApiService, private router: Router) {
+  this.reForm = this.fb.group({
+  email: ['', [Validators.required, Validators.minLength(1), Validators.email]],
+  password: ['', Validators.required],
+  name: ['', Validators.required]
+  });
   }
-  volver() {
-    this.router.navigate(['/iniciars']);
+
+  ngOnInit() {
   }
 
+  postdata(reForm1)
+  {
+  this.dataService.userregistration(reForm1.value.name, reForm1.value.email, reForm1.value.password)
+  .pipe(first())
+  .subscribe(
+  data => {
+  this.router.navigate(['/iniciars']);
+  },
+
+  error => {
+    alert("No se Ha podido Registrar")
+  });
+  }
+
+  get email() { return this.reForm.get('email'); }
+  get password() { return this.reForm.get('password'); }
+  get name() { return this.reForm.get('name'); }
 }
